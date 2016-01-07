ce9c04c6f0aac0a1e100d73a0fc77e5a81769c3b
navyxie2010@gmail.com <951178609.com>
2016-01-08 04:24:49 +0800

    game demo

--- examples/earth/js/game.js
+++ examples/earth/js/game.js
@@ -18,13 +18,18 @@
 			this.create = function(){
 				this.init();
 				this.createScore();
-				this.createEarth();
-				this.createBaffle();
+				this.createEarth();				
 				this.createGround();	
+				this.createBaffle();
 			};
 			this.init = function(){
+				game.physics.startSystem(Phaser.Physics.ARCADE);
 				this.over = false;
 				this.score = 0;
+				this.velocity = {
+					x:100,
+					y:100
+				}
 			}
 			this.createScore = function(){			
 				this.scoreText = game.add.text(5,5,'score:'+this.score,{fontSize:'22px',fill:'#000'});
@@ -32,30 +37,36 @@
 			this.createEarth = function(){
 				this.earth = game.add.sprite(game.width/2,0,'earth');
 				game.physics.enable(this.earth,Phaser.Physics.ARCADE);
+				this.earth.enableBody = true;
 				this.earth.body.collideWorldBounds = true;
-				this.earth.body.gravity.y = 100;
 				this.earth.anchor.setTo(0.5,0);
+				this.updateEarth(this.velocity.x,this.velocity.y);
 			};
 			this.createGround = function(){
 				this.ground = game.add.sprite(0,game.height,'ground');
+				this.ground.enableBody = true;
 				game.physics.enable(this.ground,Phaser.Physics.ARCADE);
 				this.ground.scale.setTo(1.2,1);
-				this.ground.anchor.setTo(0,1);			
+				this.ground.anchor.setTo(0,0.5);			
 				this.ground.body.immovable = true;
 			};
 			this.createBaffle = function(){
-				this.baffle = game.add.sprite(game.width/2,game.height - 112,'baffle');
+				this.baffle = game.add.sprite(game.width/2,game.height - this.ground.body.height/2,'baffle');
 				game.physics.enable(this.baffle,Phaser.Physics.ARCADE);
+				this.baffle.enableBody = true;
 				this.baffle.body.collideWorldBounds = true;
 				this.baffle.body.immovable = true;
 				this.baffle.anchor.setTo(0.5,1);
 				this.baffle.scale.setTo(0.1,0.1);
 			};
 			this.update = function(){
-				game.physics.arcade.collide(this.earth,this.baffle);
-				game.physics.arcade.collide(this.earth,this.ground);
-				game.physics.arcade.overlap(this.earth,this.ground,this.gameOver, null, this);
-				game.physics.arcade.overlap(this.earth,this.baffle,this.updateScore, null, this);
+				if(this.over){
+					this.updateEarth(0,0);
+					this.baffle.body.velocity.x = 0;
+					return;
+				}
+				game.physics.arcade.collide(this.earth,this.baffle,this.updateScore, null, this);
+				game.physics.arcade.collide(this.earth,this.ground,this.gameOver, null, this);
 				var cursors = game.input.keyboard.createCursorKeys();
 				if(cursors.left.isDown){
 					this.baffle.body.velocity.x = -150;
@@ -64,20 +75,60 @@
 				}else{
 					this.baffle.body.velocity.x = 0;
 				}
+				this.checkBounds();
 			};
+			this.checkBounds = function(){
+				if(this.over){
+					return;
+				}
+				var position = this.earth.position;
+				var change = false;
+				if(position.x <= 12){
+					this.velocity.x = Math.abs(this.velocity.x);
+					change = true;
+				}
+				if(position.x >= (game.width -12)){
+					this.velocity.x = -Math.abs(this.velocity.x);
+					change = true;
+				}
+				if(position.y <= 0){
+					this.velocity.y = Math.abs(this.velocity.y);
+					change = true;
+				}
+				if(position.y >= (game.height -22)){
+					this.velocity.y = -Math.abs(this.velocity.y);
+					change = true;
+				}
+				if(change){
+					this.updateEarth(this.velocity.x,this.velocity.y);
+				}
+			}
 			this.gameOver = function(){
-				console.log('gameOver');
 				if(this.over){
 					return;
 				}				
 				this.over = true;
 			};
 			this.updateScore = function(){
-				console.log('updateScore');
 				if(!this.over){
 					this.score++;
 					this.scoreText.text = 'score: ' + this.score;
+					if(Math.ceil(Math.random()*2)%2 === 0){
+						this.velocity.x = -this.velocity.x;
+						this.velocity.x -= (10 + Math.ceil(Math.random()*10));
+					}else{
+						this.velocity.x += (10 + Math.ceil(Math.random()*10));
+					}				
+					this.velocity.y = -this.velocity.y;
+					this.velocity.y -= 10;
+					this.updateEarth(this.velocity.x,this.velocity.y);
 				}
+			};
+			this.updateEarth = function(x,y){
+				x = x || 0;
+				y = y || 0;
+				this.velocity.x = this.earth.body.velocity.x = x;
+				this.velocity.y = this.earth.body.velocity.y = y;	
 			}
 		}
 	}
