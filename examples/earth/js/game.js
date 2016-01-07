3f02eeb6fcacabf3ccfef177794c26542dee7b80
navyxie2010@gmail.com <951178609.com>
2016-01-08 03:04:27 +0800

    update sprite

--- examples/earth/js/game.js
+++ examples/earth/js/game.js
@@ -3,10 +3,10 @@
 	var states = {
 		boot:function(){
 			this.preload = function(){
-				game.load.image('earth','assets/earth.png');
+				game.load.image('earth','assets/star.png');
 				game.load.image('ground','assets/ground.png');
-				game.load.image('baffle','assets/preloader.gif');
-			},
+				game.load.image('baffle','assets/platform.png');
+			};
 			this.create = function(){
 				game.state.start('play');
 			}
@@ -14,13 +14,16 @@
 		play:function(){
 			this.preload = function(){
 				game.stage.backgroundColor = 0x078CFD;
-			},
+			};
 			this.create = function(){
-				// game.physics.startSystem(Phaser.Physics.ARCADE);
+				this.over = false;
+				this.score = 0;
+				this.scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+				game.physics.startSystem(Phaser.Physics.ARCADE);
 				this.earth = game.add.sprite(game.width/2,0,'earth');
 				game.physics.enable(this.earth,Phaser.Physics.ARCADE);
 				this.earth.body.collideWorldBounds = true;
-				this.earth.scale.setTo(0.08,0.08);
+				//this.earth.scale.setTo(0.08,0.08);
 				this.earth.body.gravity.y = 100;
 				this.earth.anchor.setTo(0.5,0);
 				this.ground = game.add.sprite(0,game.height,'ground');
@@ -28,15 +31,40 @@
 				this.ground.scale.setTo(1.2,1);
 				this.ground.anchor.setTo(0,1);			
 				this.ground.body.immovable = true;
-				this.baffle = game.add.sprite(game.width/2,game.height - 109,'baffle');
+				this.baffle = game.add.sprite(game.width/2,game.height - 112,'baffle');
 				game.physics.enable(this.baffle,Phaser.Physics.ARCADE);
+				this.baffle.body.collideWorldBounds = true;
 				this.baffle.body.immovable = true;
 				this.baffle.anchor.setTo(0.5,1);
-				this.baffle.scale.setTo(0.3,1);
-			},
+				this.baffle.scale.setTo(0.1,0.1);
+			};
 			this.update = function(){
 				game.physics.arcade.collide(this.earth,this.baffle);
 				game.physics.arcade.collide(this.earth,this.ground);
+				game.physics.arcade.overlap(this.earth,this.ground,this.gameOver, null, this);
+				game.physics.arcade.overlap(this.earth,this.baffle,this.updateScore, null, this);
+				var cursors = game.input.keyboard.createCursorKeys();
+				if(cursors.left.isDown){
+					this.baffle.body.velocity.x = -150;
+				}else if(cursors.right.isDown){
+					this.baffle.body.velocity.x = 150;
+				}else{
+					this.baffle.body.velocity.x = 0;
+				}
+			};
+			this.gameOver = function(){
+				if(this.over){
+					return;
+				}
+				console.log('gameOver');
+				this.over = true;
+			};
+			this.updateScore = function(){
+				console.log('updateScore');
+				if(!this.over){
+					this.score++;
+					this.scoreText.text = 'Score: ' + this.score;
+				}
 			}
 		}
 	}
