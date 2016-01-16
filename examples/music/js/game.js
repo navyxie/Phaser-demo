b4f6d7e55b8977e85b46c09b31573d396a84655d
navyxie2010@gmail.com <951178609.com>
2016-01-17 00:51:41 +0800

    add game.js

--- examples/music/js/game.js
+++ examples/music/js/game.js
@@ -1,3 +1,12 @@
 ;(function(){
-	
+	var game = new Phaser.Game(400,600,Phaser.AUTO,'game');
+	var states = {};
+	states.boot = {
+		preload:function(){
+			game.load.image('ball_kill','assets/ball_kill.png');
+			game.load.image('ball_ok','assets/ball_ok.png');
+		}
+	}
+	game.state.add('boot',states.boot);
+	game.state.start('boot');
 })();
